c240604186506dad955414ba718b7254c9fc8383
LerouxdesE2 <matteo.gosselin@mail.com>
2022-05-17 21:03:19 +0200

    gdgf

---
+++ SiteWebTemporaire/event.js
@@ -0,0 +1,26 @@
+// TODO: compresser le fichier pour assurer la sécurité
+//info: Fichier pour les éléments déclencheurs (sur les pages classiques)
+
+
+//! Changer le chemin d'accès en fonction du serveur
+if(location.pathname === "/index.html" || location.pathname === "/"){
+    console.log("pathname : True")
+    const sus = document.getElementById('ඞ')
+    if(navigator.userAgent.match (/iphone/i) || navigator.userAgent.match (/Android/i)){
+        sus.addEventListener("click", () =>{
+            console.log("GG!")
+            window.location.href = "sus/ඞ.html"
+        })
+    }else{
+        sus.addEventListener("dblclick", () =>{
+            console.log("GG!")
+            window.location.href = "sus/ඞ.html"
+        })
+    }
+}
+if(location.pathname === "/credits.html"){
+    let TheRealImage = document.getElementById('key')
+    TheRealImage.addEventListener('click', ()=>{
+        window.location.href = "sus/pour_le_fbi.html"
+    })
+}
