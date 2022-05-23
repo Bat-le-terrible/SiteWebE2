ab131af0feaf1d1a0055a63c83ec79cd0adc7896
LerouxdesE2 <matteo.gosselin@mail.com>
2022-05-23 20:29:40 +0200

    hkb

--- event.js
+++ event.js
@@ -3,41 +3,24 @@
 
 
 //! Changer le chemin d'accès en fonction du serveur
-// if(location.pathname === '/' || location.pathname === "/index.html"){
-//     console.log("pathname : True")
-//     const sus = document.getElementById('ඞ')
-//     if(navigator.userAgent.match (/iphone/i) || navigator.userAgent.match (/Android/i)){
-//         sus.addEventListener("click", () =>{
-//             console.log("GG!")
-//             window.location.href = "sus/ඞ.html"
-//         })
-//     }else{
-//         sus.addEventListener("dblclick", () =>{
-//             console.log("GG!")
-//             window.location.href = "sus/ඞ.html"
-//         })
-//     }
-// }
-// if(location.pathname === "/credits.html"){
-//     let TheRealImage = document.getElementById('key')
-//     TheRealImage.addEventListener('click', ()=>{
-//         window.location.href = "sus/pour_le_fbi.html"
-//     })
-// }
-
-
-//Event 2
-// const logo = document.getElementById('logo')
-// logo.addEventListener('hover', () =>{
-//     console.log('test')
-//     let nb = 5
-//     setInterval(()=>{
-//         for(i=0; i<5;i++){
-//             logo.title = "5"
-//             nb = nb-1
-
-//         }
-//     },1000)
-// })
-
-
+ if(location.pathname === '/' || location.pathname === "/index.html"){
+     console.log("pathname : True")
+     const sus = document.getElementById('ඞ')
+     if(navigator.userAgent.match (/iphone/i) || navigator.userAgent.match (/Android/i)){
+         sus.addEventListener("click", () =>{
+             console.log("GG!")
+             window.location.href = "sus/ඞ.html"
+         })
+     }else{
+         sus.addEventListener("dblclick", () =>{
+             console.log("GG!")
+             window.location.href = "sus/ඞ.html"
+         })
+     }
+ }
+ if(location.pathname === "/credits.html"){
+     let TheRealImage = document.getElementById('key')
+     TheRealImage.addEventListener('click', ()=>{
+         window.location.href = "sus/pour_le_fbi.html"
+     })
+ }
